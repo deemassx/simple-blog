e904ae3848128d8f7d079a8e97bdb72b4a9e8593
deemassx <contact.dimasss@gmail.com>
2023-08-24 11:42:33 +0700

    create request handler method

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // load http module
 const http = require('http');
 
+// create method to handle request 
+function requestHandler(request, response){
+    response.statusCode = 200;
+    response.write('Hello World');
+    response.end();
+};
+
 // create server
-const server = http.createServer();
+const server = http.createServer(requestHandler);
 
 // running server 
 server.listen(3000, function(){
