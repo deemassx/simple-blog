12cfa0e76d964026065dc0b3d8e23550f1ff376a
deemassx <contact.dimasss@gmail.com>
2023-08-29 09:48:40 +0700

    create route

--- app.js
+++ app.js
@@ -3,9 +3,25 @@ const http = require('http');
 
 // create method to handle request 
 function requestHandler(request, response){
-    response.statusCode = 200;
-    response.write('Hello World');
-    response.end();
+
+    const url = request.url;
+
+    switch (url) {
+        case "/":
+            response.statusCode = 200;
+            response.write('Hello World');  
+            response.end();       
+            break;
+        case "/about":
+            response.statusCode = 200;
+            response.write('This is my own page');
+            response.end();
+            break; 
+        default:
+            response.statusCode = 404;
+            response.write('The page was not found');
+            response.end();    
+    }
 };
 
 // create server
